a0a530c12060409a969b2f814e7f8d723aeb5947
Eric Chang <a1756972831@gmail.com>
2025-02-22 14:49:06 +0800

    feat: Add CourseFactory and update TeacherFactory for seeding

--- database/factories/TeacherFactory.php
+++ database/factories/TeacherFactory.php
@@ -17,7 +17,8 @@ class TeacherFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
         ];
     }
 }
