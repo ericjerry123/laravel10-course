a8d0f7eef5a3d4c9d54fe52222617a06a87abb29
Eric Chang <a1756972831@gmail.com>
2025-02-22 15:38:31 +0800

    feat: implement get all course

--- app/Services/CourseService.php
+++ app/Services/CourseService.php
@@ -13,6 +13,22 @@ class CourseService
         $this->courseRepository = $courseRepository;
     }
 
+    /**
+     * 取得所有課程
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getAll()
+    {
+        return $this->courseRepository->getAll();
+    }
+
+    /**
+     * 新增課程
+     *
+     * @param array $data
+     * @return \App\Models\Course
+     */
     public function create(array $data)
     {
         return $this->courseRepository->create($data);
